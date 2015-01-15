2a40024483ce813c5b6dc4c4f562388e64dda00f
Lucas Holmquist <lholmqui@redhat.com>
2015-01-15 14:42:39 -0500

    perhaps better?

--- SwapiiOS/swapi.swift
+++ SwapiiOS/swapi.swift
@@ -10,7 +10,7 @@ import Foundation
 import SwiftyJSON
 
 public class Swapi {
-    var baseURL = "http://swapi.co/a/"
+    var baseURL = "http://swapi.co/api/"
     var session: NSURLSession
     
     public init() {
@@ -21,34 +21,35 @@ public class Swapi {
         self.session.finishTasksAndInvalidate()
     }
     
-    public func getPerson(id: String, callback: (JSON, NSError?) -> Void) {
+    public func getPerson(id: String, callback: (AnyObject!, NSError?) -> Void) {
         makeRequest("people/" + id, callback)
     }
     
-    public func getStarship(id: String, callback: (JSON, NSError?) -> Void) {
+    public func getStarship(id: String, callback: (AnyObject!, NSError?) -> Void) {
         makeRequest("starship/" + id, callback)
     }
     
-    public func getVehicle(id: String, callback: (JSON, NSError?) -> Void) {
+    public func getVehicle(id: String, callback: (AnyObject!, NSError?) -> Void) {
         makeRequest("vehicle/" + id, callback)
     }
     
-    public func getFilm(id: String, callback: (JSON, NSError?) -> Void) {
+    public func getFilm(id: String, callback: (AnyObject!, NSError?) -> Void) {
         makeRequest("films/" + id, callback)
     }
     
-    public func getSpecies(id: String, callback: (JSON, NSError?) -> Void) {
+    public func getSpecies(id: String, callback: (AnyObject!, NSError?) -> Void) {
         makeRequest("species/" + id, callback)
     }
     
-    private func makeRequest(url: String, callback: (JSON, NSError?) -> Void) {
+    private func makeRequest(url: String, callback: (AnyObject!, NSError?) -> Void) {
         let request = NSMutableURLRequest(URL: NSURL(string: baseURL + url + "/")!)
         
         let task = session.dataTaskWithRequest(request, completionHandler: {(data, response, error) -> Void in
             if (error != nil) {
-                callback(nil, error)
+                callback("Error", error)
             } else {
-                callback(JSON(data: data), nil);
+                let jsonObject: AnyObject! = JSON(data: data).dictionaryObject!
+                callback(jsonObject, nil)
             }
         })
         
